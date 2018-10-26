c37702c5e643e460bfdea4df99188b5f95a49343
Joel Kåberg <joel@kaberg.me>
2018-10-26 09:05:17 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# trv-ical-server
+an simple ical server for Trondheim Renholdsverk (converting html to iCal)
+
+my personal insance is running at https://trv-ical.eth0.sh
---
+++ main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+from flask import Flask, Response, request
+from ics import Calendar, Event
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'usage: {0}*plan_id*.ics<br /><br />for plan_id see: https://trv.no > search > its the latter part of the url (numeric)'.format(request.base_url)
+
+@app.route('/<plan_id>.ics')
+def calendar(plan_id):
+    cal = str(fetch_plan(plan_id))
+    hax = cal.split('\n')
+    
+    # hax 2k18 :-)
+    hax.insert(2, 'X-FROM-URL:{0}\r'.format(request.base_url))
+    hax.insert(3, 'X-WR-RELCALID:TRV Tømmeplan\r')
+    hax.insert(4, 'X-WR-CALNAME:TRV Tømmeplan\r')
+    hax.insert(5, 'X-WR-TIMEZONE:Europe/Oslo\r')
+    resp = '\n'.join(hax)
+
+    return Response(resp, mimetype='text/calendar')
+
+def fetch_plan(plan_id):
+    url = 'https://trv.no/plan/{0}/'.format(plan_id)
+    page = requests.get(url).text
+
+    soup = BeautifulSoup(page, 'html.parser')
+    table = soup.find("table", {"class": "bins"})
+    c = Calendar()
+    c.creator = 'https://github.com/jkaberg/trv-ical-server'
+
+    for table_row in table.select('tbody tr'):
+        year_data = table_row["class"]
+
+        cells = table_row.findAll('td')
+
+        if len(cells) > 0:
+            year = year_data[0].split('-')[1]
+
+            if len(year_data) > 1 and year_data[1] == 'first-of-new-year':
+                start_year = int(year) - 1
+            else:
+                start_year = year
+
+            dates = cells[2].text.split(' - ')
+            week = cells[0].text.strip()
+            trv_type = cells[1].text.strip()
+
+            start = dates[0].split('.')
+            end = dates[1].split('.')
+
+            e = Event()
+            e.name = trv_type
+            e.begin = '{0}{1}{2} 00:00:00'.format(start_year, start[1], start[0])
+            e.end = '{0}{1}{2} 00:00:00'.format(year, end[1], end[0])
+            c.events.add(e)
+
+    return c
