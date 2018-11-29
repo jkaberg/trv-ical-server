08c5bfcd3dd3b30c72bf74f5386cecf511c12776
Joel Kåberg <joel@kaberg.me>
2018-11-29 21:29:37 +0100

    use icalendar instead

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import requests
+from datetime import datetime
 from bs4 import BeautifulSoup
-from ics import Calendar, Event
 from flask_caching import Cache
 from flask import Flask, Response, request
+from icalendar import Calendar, Event
 
 
 app = Flask(__name__)
@@ -20,31 +21,22 @@ def hello():
 
 @app.route('/<plan_id>.ics')
 @cache.cached(timeout=cache_timeout)
-def calendar(plan_id):
-    cal = str(fetch_plan(plan_id))
-    hax = cal.split('\n')
-    
-    # hax 2k18 :-)
-    hax.insert(2, 'X-FROM-URL:{0}\r'.format(request.base_url))
-    hax.insert(3, 'X-WR-RELCALID:TRV Tømmeplan\r')
-    hax.insert(4, 'X-WR-CALNAME:TRV Tømmeplan\r')
-    hax.insert(5, 'X-WR-TIMEZONE:Europe/Oslo\r')
-    resp = '\n'.join(hax)
-
-    return Response(resp, mimetype='text/calendar')
-
 def fetch_plan(plan_id):
     url = 'https://trv.no/plan/{0}/'.format(plan_id)
     page = requests.get(url).text
 
     soup = BeautifulSoup(page, 'html.parser')
     table = soup.find("table", {"class": "bins"})
+
     c = Calendar()
-    c.creator = 'https://github.com/jkaberg/trv-ical-server'
+    c.add('X-WR-RELCALID', 'TRV Tømmeplan')
+    c.add('X-WR-CALNAME', 'TRV Tømmeplan')
+    c.add('X-WR-TIMEZONE', 'Europe/Oslo')
+    c.add('X-FROM-URL', '{0}'.format(request.base_url))
+    c.add('X-AUTHOR', 'https://github.com/jkaberg/trv-ical-server')
 
     for table_row in table.select('tbody tr'):
         year_data = table_row["class"]
-
         cells = table_row.findAll('td')
 
         if len(cells) > 0:
@@ -63,9 +55,12 @@ def fetch_plan(plan_id):
             end = dates[1].split('.')
 
             e = Event()
-            e.name = trv_type
-            e.begin = '{0}{1}{2} 00:00:00'.format(start_year, start[1], start[0])
-            e.end = '{0}{1}{2} 00:00:00'.format(year, end[1], end[0])
-            c.events.add(e)
+            e.add('description', trv_type)
+            e.add('summary', trv_type)
+            e.add('dtstart', datetime(int(start_year), int(start[1]), int(start[0])).date())
+            e.add('dtend', datetime(int(year), int(end[1]), int(end[0])).date())
+            e.add('dtstamp', datetime.now())
 
-    return c
+            c.add_component(e)
+
+    return Response(c.to_ical(), mimetype='text/calendar')
